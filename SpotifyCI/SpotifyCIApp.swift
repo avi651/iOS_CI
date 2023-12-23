dbacab18cb2f8b871f1379979310388bbdf572cc
Avinash Kumar <kumaravi651@gmail.com>
2023-12-24 01:57:22 +0530

    Initial Commit

---
+++ SpotifyCI/SpotifyCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCIApp.swift
+//  SpotifyCI
+//
+//  Created by Avinash on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
